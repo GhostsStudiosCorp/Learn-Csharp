b601b4d717737a489323fbbf404c6ac1395d406a
GuillerFuentec <copelloguillermo60@gmail.com>
2024-02-01 15:53:06 -0500

    Modulo de Metodos en C#

--- Learning-C--learn/Modulo Metodos/Ejercicio conversion de divisisas/Program.cs
+++ Learning-C--learn/Modulo Metodos/Ejercicio conversion de divisisas/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double usd = 23.73;
+int vnd = UsdToVnd(usd);
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+
+int UsdToVnd(double usd) 
+{
+    int rate = 23500;
+    return (int) (rate * usd);
+}
+
+double VndToUsd(int vnd)
+{
+    double rate = 23500;
+    return vnd / rate;
+}
