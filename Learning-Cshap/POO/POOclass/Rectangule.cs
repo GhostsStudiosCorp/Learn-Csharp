d2ad1d199c48604e0a5611e9345d91813573fc0a
GuillerFuentec <copelloguillermo60@gmail.com>
2024-02-20 19:50:54 -0500

    I'm studing

---
+++ Learning-Cshap/POO/POOclass/Rectangule.cs
@@ -0,0 +1,20 @@
+using system;
+// a class in C# is like a mold, 
+// the class is a layout that you gonna use in the future
+class Rectangule
+{
+    double height;
+    double weidth;
+    string color;
+
+    public double calculateArea()
+    {
+        return height * weidth;
+    }
+
+    public double calculatePerimeter()
+    {
+
+        return height * weidth;
+    }
+}
---
+++ Learning-Cshap/POO/POOclass/main.cs
@@ -0,0 +1,14 @@
+using system;
+
+namespace POOclass
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            Rectangule rectangule = new Rectangule();
+
+            rectangule.calculatePerimeter();
+        }
+    }
+}
